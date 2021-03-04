1eabef0f502e258100791e6bb712feb3fd410d7c
BaburinAnton <79838664+BaburinAnton@users.noreply.github.com>
2021-03-04 17:41:21 +0300

    Delete Lesson 1 homework directory

--- Lesson 1 homework/1.py
+++
@@ -1,9 +0,0 @@
-a = 5
-b = 10
-print(a)
-print(b)
-numder1 = input('Введите первое число - ')
-number2 = input('Введите второе число -')
-string1 = input('Введите строку')
-string2 = input('Введите строку')
-print('Вы ввели значения -', numder1, number2, string1, string2)
